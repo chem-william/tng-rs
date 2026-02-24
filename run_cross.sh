e1800d5a3d296daa9bd63c071f275e5631157d2b
William Bro-Jørgensen <brojorgensen@proton.me>
2026-02-24 20:20:50 +0100

    add more architectures to cross tests

--- run_cross.sh
+++ run_cross.sh
@@ -1,3 +1,7 @@
 # if a glibc error pops up, just run `cargo clean` - see https://github.com/cross-rs/cross/issues/724
-# powerpc should be big-endian 
+# powerpc64 is big-endian 64-bit
 cross test --target powerpc64-unknown-linux-gnu
+# powerpc is big-endian 32-bit
+cross test --target powerpc-unknown-linux-gnu
+# armv7 is little-endian 32-bit
+cross test --target armv7-unknown-linux-gnueabihf
