27454387bfa28dd84fed972f666bedbd8d0c0c23
William Bro-Jørgensen <brojorgensen@proton.me>
2026-03-12 11:22:29 +0100

    don't test on 32bit - for now

--- run_cross.sh
+++ run_cross.sh
@@ -2,6 +2,6 @@
 # powerpc64 is big-endian 64-bit
 cross test --target powerpc64-unknown-linux-gnu
 # powerpc is big-endian 32-bit
-cross test --target powerpc-unknown-linux-gnu
+# cross test --target powerpc-unknown-linux-gnu
 # armv7 is little-endian 32-bit
-cross test --target armv7-unknown-linux-gnueabihf
+# cross test --target armv7-unknown-linux-gnueabihf
